0b7d801a9d16db37d8698266687d647291cc3b43
Olivier Gourgue <ogourgue@gmail.com>
2020-03-03 18:55:59 -0500

    start to comment before depositing on Zenodo

--- vfc.py
+++ vfc.py
@@ -1,71 +1,175 @@
+""" VFC: Vegetation/Flow Convolution
+
+This module allows to simulate flow velocities around vegetation patches using a computationally-efficient convolution method (conceptual approach) or spatially refine flow velocities from a coarse-resolution model (practical implementation)
+
+Note: Practical approach (spatial refinement) currently only available for unstructured triangular grid models
+
+Reference:
+  Gourgue, O., van Belzen, J., Schwarz, C., Bouma, T.J., van de Koppel, J. & Temmerman, S. (2020) A convolution method to assess subgrid-scale interactions between flow and patchy vegetation in biogeomorphic models, Journal of Advances in Modeling Earth Systems, submitted.
+
+"""
+
 import numpy as np
 from scipy.ndimage import rotate
 from scipy.signal import fftconvolve
 
 
+
+################################################################################
+# conceptual approach ##########################################################
 ################################################################################
 
-def convolution_simple(U0, X, Y, V, M, beta = .5, fsp = 1):
+def convolution_conceptual(X, Y, V, M, beta = .5):
 
-  # vegetation and mask grid size
+  """ Calculate the magnitude of the normalized flow velocity U/U0 resulting from the vegetation distribution V and the convolution mask M (Gourgue et al., 2020, Equation 10)
+
+  Required parameters:
+  X, Y (NumPy array of size (m) and (n)): coordinates of the rectangular grid
+  V (NumPy array of size (m,n)): Vegetation distribution (equal 1 on vegetated cells, 0 otherwise)
+  M (NumPy array of size (k,l)): Convolution mask, that is, magnitude of the normalized flow velocity around an elementary vegetation patch of the size of one rectangular grid cell (Gourgue et al., 2020)
+
+  Optional parameter:
+  beta (float, default = 0.5): Convolution parameter ranging from 0 to 1 (high values accentuate the impact of the convolution mask) (Gourgue et al., 2020, Equation 11)
+
+  Returns:
+  Numpy array of size (m,n): Magnitude of the normalized flow velocity around the vegetation
+
+  """
+
+  # grid size
   DX = X[1] - X[0]
 
   # vegetation surface over convolution mask
   SV = fftconvolve(V, np.ones(M.shape), mode = 'same') * (DX ** 2)
   SV[SV < 1] = 1
 
-  # convolution product
-  CONV = np.exp(fftconvolve(V / (SV ** beta), np.log(M), mode = 'same'))
+  # return normalized velocity
+  return np.exp(fftconvolve(V / (SV ** beta), np.log(M), mode = 'same'))
 
-  # species rescaling
-  CONV = 1 + (CONV - 1) * fsp
 
-  # convolution product to velocity
-  return U0 * CONV
+
+################################################################################
+# practical implementation - step 1: conceptual approach in N directions #######
+################################################################################
+
+def convolution_step_1(X, Y, V, M, beta = .5, N = 8):
+
+  """ Calculate the normalized convoluted velocity matrix in N directions (Gourgue et al., 2020, Equation 12)
+
+  Required parameters:
+  X, Y (NumPy array of size (m) and (n)): coordinates of the rectangular grid
+  V (NumPy array of size (m,n)): Vegetation distribution (equal 1 on vegetated cells, 0 otherwise)
+  M (NumPy array of size (k,l)): Convolution mask, that is, magnitude of the normalized flow velocity around an elementary vegetation patch of the size of one rectangular grid cell (Gourgue et al., 2020)
+
+  Optional parameters:
+  beta (float, default = 0.5): Convolution parameter ranging from 0 to 1 (high values accentuate the impact of the convolution mask) (Gourgue et al., 2020, Equation 11)
+  N (int, default = 8): Number of convolution directions
+
+  Returns:
+  Numpy array of size (N,m,n): Normalized convoluted velocity matrix in N directions
+
+  """
+
+  # bin size
+  dtheta = 360. / N
+
+  # convolution directions
+  THETA = np.linspace(0., 360., N, endpoint = False)
+
+  # initialize
+  CONV = np.zeros((N, V.shape[0], V.shape[1]))
+
+  # convolution in each direction
+  for i in range(N):
+
+    # rotate convolution mask
+    Mi = rotate(M, THETA[i], mode = 'constant', cval = 1)
+
+    # conceptual approach
+    CONV[i, :, :] = convolution_conceptual(X, Y, V, Mi, beta)
+
+  # return normalized convoluted velocity matrix in N directions
+  return CONV
+
+
+
+################################################################################
+# practical implementation - step 2: aggregation of directional maps ###########
+################################################################################
+
+def convolution_step_2(x, y, tri, u, v, X, Y, V, M, beta = .5, N = 8):
+
+  """ Calculate the normalized aggregated velocity matrix (Gourgue et al., 2020, Equation 12)
+
+  Required parameters:
+  x, y (NumPy arrays of size (npoin)): coordinates of the triangular grid
+  tri (NumPy array of size (npoin, 3)): connectivity table of the triangular grid
+  u, v (NumPy arrays of size (npoin)): coarse-resolution flow velocities on the triangular grid
+  X, Y (NumPy array of size (m) and (n)): coordinates of the rectangular grid
+  V (NumPy array of size (m,n)): Vegetation distribution (equal 1 on vegetated cells, 0 otherwise)
+  M (NumPy array of size (k,l)): Convolution mask, that is, magnitude of the normalized flow velocity around an elementary vegetation patch of the size of one rectangular grid cell (Gourgue et al., 2020)
+
+  Optional parameters:
+  beta (float, default = 0.5): Convolution parameter ranging from 0 to 1 (high values accentuate the impact of the convolution mask) (Gourgue et al., 2020, Equation 11)
+  N (int, default = 8): Number of convolution directions
+
+  Returns:
+  Numpy array of size (m,n): Normalized aggregated velocity matrix
+  NumPy array of size (m,n): Voronoi matrix
+
+  """
+
+  # bin size
+  dtheta = 360. / N
+
+  # central bin direction
+  THETA = np.linspace(0., 360., N, endpoint = False)
+
+  # convolution step 1
+  CONV1 = convolution_step_1(X, Y, V, M, beta = beta, N = N)
+
+  # coarse-resolution flow directions and corresponding convolution bins
+  theta = np.degrees(np.arctan2(v, u))
+  bin = np.zeros(theta.shape, dtype = 'int')
+  for i in range(N):
+    bin[theta > THETA[i] - dtheta / 2] = i
+  bin[theta > THETA[-1] + dtheta / 2] = 0
+
+  # voronoi neighborhood
+  VOR = voronoi(x, y, tri, X, Y)
+
+  # select appropriate convolution bin for each voronoi cell
+  CONV = np.zeros(V.shape)
+  for i in range(N):
+    CONV[bin[VOR] == i] = CONV1[i, :, :][bin[VOR] == i]
+
+  # return normalized aggregated velocity and voronoi matrices
+  return CONV, VOR
 
 
 
 ################################################################################
 
-def convolution(x, y, tri, u, v, X, Y, V, M, beta = .5, fsp = 1, nbin = 8):
+def convolution_step_3(x, y, tri, u, v, X, Y, V, M, beta = .5, nbin = 8):
 
-  # vegetation and mask grid size
-  DX = X[1] - X[0]
-
-  # bin size
-  dtheta = 360. / nbin
-
-  # central bin direction
-  theta0 = np.linspace(0., 360., nbin, endpoint = False)
-
-  # convolution in each bin
-  TMP = []
-  for i in range(nbin):
-
-    # rotate mask
-    M_i = rotate(M, theta0[i], mode = 'constant', cval = 1)
-
-    # convolution simple
-    TMP.append(convolution_simple(1, X, Y, V, M_i, beta = beta, fsp = fsp))
-
-  # original flow direction and corresponding bin
-  theta = np.degrees(np.arctan2(v, u))
-  bin = np.zeros(theta.shape, dtype = 'int')
-  for i in range(nbin):
-    bin[theta > theta0[i] - dtheta / 2] = i
-  bin[theta > theta0[-1] + dtheta / 2] = 0
-
-  # voronoi neighborhood
-  VOR = voronoi(x, y, tri, X, Y)
-
-  # select appropriate bin for each voronoi cell
-  CONV = np.zeros(V.shape)
-  for i in range(nbin):
-    CONV[bin[VOR] == i] = TMP[i][bin[VOR] == i]
+  # convolution step 3
+  CONV, VOR = convolution_step_2(x, y, tri, u, v, X, Y, V, M, beta = beta, \
+                                 nbin = nbin)
 
   # conservative rescaling inside each voronoi cell
   for i in range(len(x)):
     CONV[VOR == i] /= np.mean(CONV[VOR == i])
+
+  return CONV, VOR
+
+
+################################################################################
+
+def convolution(x, y, tri, u, v, X, Y, V, M, beta = .5, nbin = 8):
+
+  # convolution step 3
+  CONV, VOR = convolution_step_3(x, y, tri, u, v, X, Y, V, M, beta = beta, \
+                                 nbin = nbin)
 
   # convolution product to velocity
   uv = ((u ** 2 + v ** 2) ** .5)
